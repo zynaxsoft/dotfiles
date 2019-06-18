37b290e3c55b5b6d819715e51b76047ffd479909
Tanapol Prucksakorn <tanapol@qbit-robotics.jp>
2019-06-18 14:17:15 +0900

    auto add submodule script

---
+++ git-auto-add-submodule.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for x in $(find . -type d) ; do
+    if [ -d "${x}/.git" ] ; then
+        cd "${x}"
+        origin="$(git config --get remote.origin.url)"
+        cd - 1>/dev/null
+        git submodule add "${origin}" "${x}"
+    fi
+done
