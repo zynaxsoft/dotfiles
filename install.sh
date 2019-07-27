8672927daa1c8f41b4c57c305893e2a3b9094a44
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-07-27 15:59:49 +0900

    xsel xclip in install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,9 @@ git submodule update
 echo "installing pylint3"
 sudo apt install pylint3
 
+echo "install clipboard tools"
+sudo apt install xsel xclip
+
 echo "installing nerd fonts"
 mkdir -p ~/.local/share/fonts
 cp fonts/dejavu-nerd-font.ttf ~/.local/share/fonts
