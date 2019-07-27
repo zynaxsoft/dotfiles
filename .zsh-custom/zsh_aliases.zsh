61467c207079768a44a7618cf388939efd4efd86
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-07-27 18:03:07 +0900

    realias ga=git add -i

--- .zsh-custom/zsh_aliases.zsh
+++ .zsh-custom/zsh_aliases.zsh
@@ -4,6 +4,8 @@ alias omore=$HOME/omotenashi/src/tools/omote-remote.py
 alias tls='tmux list-session'
 alias tks='_tmux_kill_session'
 
+alias ga='git add -i'
+
 _tmux_kill_session() {
 	tmux kill-session -t $1
 }
