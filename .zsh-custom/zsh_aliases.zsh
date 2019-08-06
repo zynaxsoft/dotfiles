9f105d9e2e99cc1db1f4cca5a7001fa53478e819
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-08-06 11:39:25 +0900

    update omore location

--- .zsh-custom/zsh_aliases.zsh
+++ .zsh-custom/zsh_aliases.zsh
@@ -1,5 +1,5 @@
 alias pact='_pacter'
-alias omore=$HOME/omotenashi/src/tools/omote-remote.py
+alias omore=$HOME/tools/omote-remote.py
 
 alias tls='tmux list-session'
 alias tks='_tmux_kill_session'
