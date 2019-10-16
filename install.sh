887b8be235a43afafe64ea6f4b294e8285d6301e
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2019-10-16 15:49:37 +0900

    add .vim link in install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ for file in .vimrc .zshrc .tmux/.tmux.conf .tmux/.tmux.conf.local .oh-my-zsh .gi
 	echo "ln -s $HOME/dotfiles/$file $HOME/"
 	ln -s $HOME/dotfiles/$file $HOME/
 done
+ln -s $HOME/dotfiles/.vim $HOME/
 git submodule init
 git submodule update
 
