9c9baa52ae3abff7e486dfbe3768b69375fd35eb
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2020-06-13 19:40:23 +0900

    replace tabs with 4 spaces in zsh_aliases.zsh

--- .zsh-custom/zsh_aliases.zsh
+++ .zsh-custom/zsh_aliases.zsh
@@ -8,22 +8,22 @@ alias ga='git add -i'
 alias rn='source ranger'
 
 _tmux_kill_session() {
-	tmux kill-session -t $1
+    tmux kill-session -t $1
 }
 
 _pacter() {
-	if [ -d ".env" ]
-	then
-		source .env/bin/activate
-	elif [ -d ".venv" ]
-	then
-		if [ -d ".venv/skill_env" ]
-		then
-			source .venv/skill_env/bin/activate
-		else
-			source .venv/bin/activate
-		fi
-	else
-		echo 'could not find .env or .venv'
-	fi
+    if [ -d ".env" ]
+    then
+        source .env/bin/activate
+    elif [ -d ".venv" ]
+    then
+        if [ -d ".venv/skill_env" ]
+        then
+            source .venv/skill_env/bin/activate
+        else
+            source .venv/bin/activate
+        fi
+    else
+        echo 'could not find .env or .venv'
+    fi
 }
