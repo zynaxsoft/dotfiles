73683070e38f7ebd2a9f9adb629190012cb64689
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-02-15 10:59:39 +0900

    add victor mono

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ echo "installing nerd fonts"
 mkdir -p ~/.local/share/fonts
 cp fonts/dejavu-nerd-font.ttf ~/.local/share/fonts
 cp fonts/fira-code-nf.ttf ~/.local/share/fonts
+cp fonts/victor-mono-medium-nf.ttf ~/.local/share/fonts
 fc-cache -f -v
 
 echo "installing git and vim"
