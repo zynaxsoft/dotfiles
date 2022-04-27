83fbb33d67419941e97d1cb264252ec02a032bfa
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-04-28 07:55:38 +0900

    remove rust analyzer first before install

--- install-lang-server.sh
+++ install-lang-server.sh
@@ -5,5 +5,6 @@ pip3 install pyright
 
 # rust analyzer
 mkdir -p ~/.local/bin
+rm -f ~/.local/bin/rust-analyzer
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
