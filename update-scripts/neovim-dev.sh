60eb2aea31c870c7d0c491b0047c0562050b3bfd
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-05-25 07:57:18 +0900

    add yes

--- update-scripts/neovim-dev.sh
+++ update-scripts/neovim-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo -E add-apt-repository --remove ppa:neovim-ppa/stable
+sudo -E add-apt-repository -y --remove ppa:neovim-ppa/stable
 sudo -E add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt update
-sudo apt upgrade neovim
+sudo apt upgrade -y neovim
