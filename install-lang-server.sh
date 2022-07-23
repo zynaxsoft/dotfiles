f7153eacb782a955e4b8e6cb1c2bdeb3726d2e65
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-07-23 11:10:28 +0900

    add lua formatter linter

--- install-lang-server.sh
+++ install-lang-server.sh
@@ -9,5 +9,10 @@ rm -f ~/.local/bin/rust-analyzer
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
 
+# lua
+sudo apt install -y luacheck
+cargo install stylua
+cargo install selene
+
 # taplo
 # cargo install --features lsp --locked taplo-cli
