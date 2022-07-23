c31b9f78dc4299057968de477ff27308d157d94c
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-07-23 23:42:47 +0900

    add update script

---
+++ update-scripts/neovim-lua-migrate-2022.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/bin/fzf
+~/.local/bin/fzf/install --all
+rm -rf ~/dotfiles/nvim/plugged
+rm -rf ~/dotfiles/nvim/autoload/vim-plugged
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+vim +PackerSync
