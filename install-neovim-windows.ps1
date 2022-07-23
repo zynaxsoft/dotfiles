990dca22e4288f5a130ad070ceeec84be6d7884d
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-07-24 08:57:43 +0900

    add mingw

--- install-neovim-windows.ps1
+++ install-neovim-windows.ps1
@@ -1,3 +1,4 @@
+choco install mingw
 choco install neovim --pre
 New-Item -ItemType SymbolicLink -Target "C:\Users\zynax\dotfiles\nvim\" -Path "C:\Users\zynax\AppData\Local\nvim"
 git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
