642a06c747b305f5dccea8d88f7ef2469361f6c0
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-07-26 09:49:06 +0900

    restore netrw

--- nvim/lua/disable-plugins.lua
+++ nvim/lua/disable-plugins.lua
@@ -1,9 +1,5 @@
 -- https://www.reddit.com/r/neovim/comments/opipij/guide_tips_and_tricks_to_reduce_startup_and/
 local disabled_built_ins = {
-  "netrw",
-  "netrwPlugin",
-  "netrwSettings",
-  "netrwFileHandlers",
   'gzip',
   'zip',
   'zipPlugin',
