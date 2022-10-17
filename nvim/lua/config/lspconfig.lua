1c7e9c50a6f3f8269809ddd0b812daf966b022b6
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2022-10-17 09:16:09 +0900

    remove deprecation method

--- nvim/lua/config/lspconfig.lua
+++ nvim/lua/config/lspconfig.lua
@@ -64,7 +64,7 @@ end
 local lspconfig = require 'lspconfig'
 
 local capabilities =
-  require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 lspconfig.pyright.setup {
   on_attach = on_attach,
