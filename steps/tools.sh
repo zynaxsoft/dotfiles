871fd302d5770f192528b873786880581468e933
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2023-03-08 11:31:01 +0900

    Fix fzf path

--- steps/tools.sh
+++ steps/tools.sh
@@ -6,7 +6,8 @@ mkdir -p ~/.local/bin
 ln -s $(which fdfind) ~/.local/bin/fd
 
 # FZF
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/bin/fzf
-~/.local/bin/fzf/install --all
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/fzf
+~/.local/fzf/install --all
+ln -s $HOME/.local/fzf/bin/fzf $HOME/.local/bin/fzf
 
 sudo apt install -y ranger gnome-tweaks xsel xclip
