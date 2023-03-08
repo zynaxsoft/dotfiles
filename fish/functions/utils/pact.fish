fe8550479d5eed97b7dfa06d470c611577461421
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2023-03-08 12:42:23 +0900

    Add new plugins

--- fish/functions/utils/pact.fish
+++ fish/functions/utils/pact.fish
@@ -1,8 +1,8 @@
 function pact
     if test -d ".env"
-        source .env/bin/activate
+        source .env/bin/activate.fish
     else if test -d ".venv"
-        source .venv/bin/activate
+        source .venv/bin/activate.fish
     else
         echo 'could not find .env or .venv'
     end
--- steps/shell_and_terminals.sh
+++ steps/shell_and_terminals.sh
@@ -5,6 +5,10 @@ sudo apt install -y tmux
 
 echo "installing fish"
 sudo apt install fish
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher update
+fish_config theme choose "Catppuccin Macchiato"
+yes | fish_config theme save
 
 echo "install starship prompt"
 curl -fsSL https://starship.rs/install.sh | sh -s -- -y
