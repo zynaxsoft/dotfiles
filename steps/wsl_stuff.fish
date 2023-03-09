8aa3bc544545aba528ae1e31f19ccd51bc7f5d45
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2023-03-10 08:03:11 +0900

    Link wsl

--- steps/wsl_stuff.fish
+++ steps/wsl_stuff.fish
@@ -3,6 +3,6 @@
 if uname -r | grep -qi microsoft
   echo "backing up wsl.conf to wsl.conf.bak"
   sudo cp -f /etc/wsl.conf /etc/wsl.conf.bak 2> /dev/null || true
-  echo "copying wsl.conf to /etc/"
-  sudo cp -f $HOME/dotfiles/wsl.conf /etc/wsl.conf
+  echo "linking wsl.conf to /etc/"
+  sudo ln -s $HOME/dotfiles/wsl.conf /etc/wsl.conf
 end
