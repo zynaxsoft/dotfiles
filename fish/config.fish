a298fda757235f00984905ba7c7ee9c10aec9b83
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2023-03-10 08:03:46 +0900

    Correct the path settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,13 @@
 set -a fish_function_path ~/dotfiles/fish/functions
 set -a fish_complete_path ~/dotfiles/fish/completions
 
-source ~/dotfiles/fish/configs/path.fish
+# PATH stuff
+fish_add_path ~/.cargo/bin
+
+if test -z $DISPLAY
+    set -gx DISPLAY :1
+    tmux set-option -g update-environment DISPLAY
+end
 
 if status is-interactive
     source ~/dotfiles/fish/configs/greetings.fish
