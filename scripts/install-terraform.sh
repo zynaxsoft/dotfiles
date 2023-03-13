5b74523d64f38b5235454fca469fbed2c2f573b6
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2023-03-13 09:27:50 +0900

    update terraform install script

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+curl -fsSL https://apt.releases.hashicorp.com/gpg | \
+    sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture)\
+    signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
