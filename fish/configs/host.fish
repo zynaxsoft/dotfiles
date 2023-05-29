7b934a4cb7f02ffc5a6a07825d537d12f4338add
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2023-05-30 08:26:50 +0900

    proper custom host

--- fish/configs/host.fish
+++ fish/configs/host.fish
@@ -1,2 +1,8 @@
+# Just copy the two lines below and put them in custom_host.fish
 set -gx MY_HOST_ICON 🚀
 set -gx MY_HOST_ALIAS (hostname)
+
+set custom_host_file ~/dotfiles/fish/configs/custom_host.fish
+if test -f $custom_host_file
+    source $custom_host_file
+end
