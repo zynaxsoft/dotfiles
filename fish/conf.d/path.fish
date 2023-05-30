85a1b9508fe4e16b0f45408c48f28ed7d169800d
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2023-05-30 17:22:36 +0900

    add local bin

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -2,4 +2,4 @@ set -a fish_function_path ~/dotfiles/fish/functions
 set -a fish_complete_path ~/dotfiles/fish/completions
 
 # PATH stuff
-fish_add_path ~/.cargo/bin
+fish_add_path ~/.cargo/bin ~/.local/bin
