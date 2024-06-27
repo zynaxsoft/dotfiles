586d0b215e3118aa960c82432225e982bf0e1a85
Tanapol Prucksakorn <me@tanapol.dev>
2024-06-27 11:29:05 +0900

    ghcup path

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -2,4 +2,4 @@ set -p fish_function_path ~/dotfiles/fish/functions
 set -p fish_complete_path ~/dotfiles/fish/completions
 
 # PATH stuff
-fish_add_path ~/.cargo/bin ~/.local/bin
+fish_add_path ~/.cargo/bin ~/.local/bin ~/.ghcup/bin
