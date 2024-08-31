5c42b10286641c0a7f6f7058ff8adb17f390bbca
Tanapol Prucksakorn <me@tanapol.dev>
2024-08-31 13:36:46 +0900

    move to sources

--- fish/configs/sources.fish
+++ fish/configs/sources.fish
@@ -2,4 +2,7 @@ starship init fish | source
 if type -q zoxide
     zoxide init fish | source
 end
+if type -q fnm
+    fnm env --use-on-cd --shell fish | source
+end
 eval $(ssh-agent -c) > /dev/null
