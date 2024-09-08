24f2f336350963e718cd3779a68d64895361d416
Tanapol Prucksakorn <me@tanapol.dev>
2024-09-08 17:52:30 +0900

    bun

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -3,3 +3,8 @@ set -p fish_complete_path ~/dotfiles/fish/completions
 
 # PATH stuff
 fish_add_path ~/.cargo/bin ~/.local/bin ~/.ghcup/bin
+
+# bun
+if test -d ~/.bun
+    fish_add_path ~/.bun/bin
+end
