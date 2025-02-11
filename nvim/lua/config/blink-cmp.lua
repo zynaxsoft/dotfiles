7ec82abe24485420eebd6a16d439ae0424850909
Tanapol Prucksakorn <me@tanapol.dev>
2025-02-11 17:47:48 +0900

    tune scores

--- nvim/lua/config/blink-cmp.lua
+++ nvim/lua/config/blink-cmp.lua
@@ -17,13 +17,16 @@ local opts = {
       'lazydev',
     },
     providers = {
+      lsp = { score_offset = 1 },
       emoji = {
         module = 'blink-emoji',
         name = 'Emoji',
-        score_offset = 15, -- Tune by preference
+        score_offset = -1, -- Tune by preference
+        min_keyword_length = 3,
+        max_items = 5,
         opts = { insert = true }, -- Insert emoji (default) or complete its name
       },
-      ripgrep = { module = 'blink-ripgrep', name = 'Ripgrep' },
+      ripgrep = { module = 'blink-ripgrep', name = 'Ripgrep', max_items = 5, min_keyword_length = 3 },
       dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
       lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
       dictionary = {
