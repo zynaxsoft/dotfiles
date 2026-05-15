38d7e5908623844511cb276c29cf9474d14f0570
Tanapol Prucksakorn <tanapol.prucksakorn@exwzd.com>
2026-05-15 18:37:48 +0900

    temp fix
    Signed-off-by: Tanapol Prucksakorn <tanapol.prucksakorn@exwzd.com>

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -30,7 +30,11 @@ function treesitter.context()
     -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
     separator = nil,
     zindex = 20, -- The Z-index of the context window
-    on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+    on_attach = function(buf)
+      -- Skip markdown: triggers a nightly Neovim treesitter crash
+      -- (neovim/neovim#39032, closed "not planned")
+      return vim.bo[buf].filetype ~= 'markdown'
+    end,
   }
 end
 
